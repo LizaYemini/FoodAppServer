550917222eab599786092128487e57865df2bf3c
Liza Yemini <Liza091995@hotmail.com>
2020-12-13 12:01:53 +0200

    added base projects

---
+++ InfraContracts/Interfaces/IInfraDal.cs
@@ -0,0 +1,16 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace InfraContracts.Interfaces
+{
+    public interface IInfraDal
+    {
+        public OracleParameter GetParameter(string paramName, OracleDbType paramType
+            , object paramValue);
+
+        public DataSet ExecSpQuery(OracleCommand cmd);
+    }
+}
---
+++ InfraDal/InfraDalImpl.cs
@@ -0,0 +1,34 @@
+﻿using InfraContracts.Interfaces;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace InfraDal
+{
+    public class InfraDalImpl: IInfraDal
+    {
+        public DataSet ExecSpQuery(OracleCommand cmd)
+        {
+            var retval = new DataSet();
+            var outParam = new OracleParameter();
+            cmd.CommandType = CommandType.StoredProcedure;
+            outParam.ParameterName = "p_RETVAL";
+            outParam.OracleDbType = OracleDbType.RefCursor;
+            outParam.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(outParam);
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            da.Fill(retval);
+            return retval;
+        }
+
+        public OracleParameter GetParameter(string paramName, OracleDbType paramType
+            , object paramValue)
+        {
+           return new OracleParameter
+            {
+                ParameterName = paramName,
+                OracleDbType = paramType,
+                Value = paramValue
+            };
+        }
+    }
+}
